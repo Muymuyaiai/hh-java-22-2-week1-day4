320679bdbbed85db8b31b3cbfe30e8950f9fdf6a
leonmaar <myu@gmx.net>
2022-09-01 15:43:21 +0200

    Alarm and Fac final2

--- src/main/java/Fac.java
+++ src/main/java/Fac.java
@@ -1,19 +1,27 @@
 public class Fac {
-    public static int fac(int nbr){
+    public static int fac(int n){
         int prod = 1;
-        for (int i = 1; i <= nbr; ++i) {
+        for (int i = 1; i <= n; i++) {
             prod *= i;
         }
         return prod;
     }
 
-    public static int fac2(int nbr){
+    public static int fac2(int n){
         int i = 1;
         int prod = 1;
-        while (i <= nbr){
+        while (i <= n){
             prod *= i;
             ++i;
         }
         return prod;
+    }
+
+    public static int fac3(int n){
+        if (n <= 1){
+            return 1;
+        } else {
+            return (fac3(n-1) * n);
+        }
     }
 }
--- src/test/java/FacTest.java
+++ src/test/java/FacTest.java
@@ -6,28 +6,54 @@ public class FacTest {
     @Test
     void fac3 (){
         int i = 3;
-        int actual = Fac.fac2(i);
+        int actual = Fac.fac(i);
         assertEquals(6, actual);
     }
 
     @Test
     void fac4 (){
         int i = 4;
-        int actual = Fac.fac2(i);
+        int actual = Fac.fac(i);
         assertEquals(24, actual);
     }
 
     @Test
     void fac12 (){
         int i = 12;
-        int actual = Fac.fac2(i);
+        int actual = Fac.fac(i);
         assertEquals(479001600, actual);
     }
 
     @Test
     void fac0 (){
         int i = 0;
-        int actual = Fac.fac2(i);
+        int actual = Fac.fac(i);
         assertEquals(1, actual);
+    }
+
+    @Test
+    void facAll (){
+        int[] nbr = {1,1,2,6,24,120,720};
+        for (int i = 0; i <= (nbr.length - 1) ; i++){
+            int actual = Fac.fac(i);
+            assertEquals(nbr[i], actual);
+        }
+    }
+
+    @Test
+    void fac2All (){
+        int[] nbr = {1,1,2,6,24,120,720};
+        for (int i = 0; i <= (nbr.length - 1) ; i++){
+            int actual = Fac.fac2(i);
+            assertEquals(nbr[i], actual);
+        }
+    }
+    @Test
+    void fac3All (){
+        int[] nbr = {1,1,2,6,24,120,720};
+        for (int i = 0; i <= (nbr.length - 1) ; i++){
+            int actual = Fac.fac3(i);
+            assertEquals(nbr[i], actual);
+        }
     }
 }
