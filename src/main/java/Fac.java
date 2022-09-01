da05cf44819bd0f7b4e07444e78fb64bd9a0abca
leonmaar <myu@gmx.net>
2022-09-01 12:28:34 +0200

    Alarm and Fac final

--- src/main/java/Fac.java
+++ src/main/java/Fac.java
@@ -2,7 +2,17 @@ public class Fac {
     public static int fac(int nbr){
         int prod = 1;
         for (int i = 1; i <= nbr; ++i) {
-            prod = prod * i;
+            prod *= i;
+        }
+        return prod;
+    }
+
+    public static int fac2(int nbr){
+        int i = 1;
+        int prod = 1;
+        while (i <= nbr){
+            prod *= i;
+            ++i;
         }
         return prod;
     }
--- src/test/java/FacTest.java
+++ src/test/java/FacTest.java
@@ -6,7 +6,28 @@ public class FacTest {
     @Test
     void fac3 (){
         int i = 3;
-        int actual = Fac.fac(i);
+        int actual = Fac.fac2(i);
         assertEquals(6, actual);
+    }
+
+    @Test
+    void fac4 (){
+        int i = 4;
+        int actual = Fac.fac2(i);
+        assertEquals(24, actual);
+    }
+
+    @Test
+    void fac12 (){
+        int i = 12;
+        int actual = Fac.fac2(i);
+        assertEquals(479001600, actual);
+    }
+
+    @Test
+    void fac0 (){
+        int i = 0;
+        int actual = Fac.fac2(i);
+        assertEquals(1, actual);
     }
 }
